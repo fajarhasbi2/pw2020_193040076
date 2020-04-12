89228b32b5ddd08a162a54409fa39f23fce9c488
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2020-04-12 17:33:52 +0700

    penambahan materialize

--- praktikum/P5_PW_193040076/latihan5c/index.php
+++ praktikum/P5_PW_193040076/latihan5c/index.php
@@ -10,21 +10,60 @@
 <html>
     <head>
     
-        <title>latihan5c</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="assets/css/materialize.min.css"  media="screen,projection"/>
 
-        <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <!-- My css -->
+      <link rel="stylesheet" href="assets/css/style.css">
+
+        <title>latihan5c</title>
 
     </head>
     
 <body>
-<div class="container">
-        <?php foreach ($laptop as $lap) :?>
-            <p class="tipe">
-                 <a href="php/detail.php?id=<?= $lap['id'] ?>">
-                    <?= $lap["tipe"] ?>
-                 </a>
-            </p>
-        <?php endforeach; ?>
-    </div>
+<!-- ini pake navbar -->
+        <div class="navbar-fixed">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="nav-wrapper">
+                        <a href="#" class="brand-logo center">Laptop Gaming</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+<!-- menggunakan collection -->
+            <div class="container">
+                <div class="collection">
+                    <h3 class=" center light  grey-text text-darken-3 "><b>List Laptop</b></h3>
+                    <?php foreach ($laptop as $lap) :?>
+                        <p class="tipe">
+                            <a href="php/detail.php?id= <?= $lap['id'] ?> " class="collection-item ">  
+                                <?= $lap["tipe"] ?>
+                            </a>
+                        </p>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+
+
+
+
+
+     <!--JavaScript at end of body for optimized loading-->
+     <script type="text/javascript" src="aseets/js/materialize.min.js"></script>
+    <script>
+        const sideNav = document. querySelectorAll('.sidenav');
+          M.Sidenav.init(sideNav);
+
+        const parallax = document. querySelectorAll('.parallax');
+        M.Parallax.init(parallax);
+
+    </script>
 </body>
 </html>
