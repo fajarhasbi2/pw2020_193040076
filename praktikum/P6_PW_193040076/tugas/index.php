637b5794a72b38aaed7d1be71fcb60011af15959
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2020-04-20 23:10:08 +0700

    updeat

--- praktikum/P6_PW_193040076/tugas/index.php
+++ praktikum/P6_PW_193040076/tugas/index.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
 
         <!-- my css -->
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="">
 
         <title>Tugas</title>
        
@@ -27,44 +27,51 @@
 <body>
 
     <!-- menggunakan navbar -->
-        <div class="container ">
-            <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
-                <a class="navbar-brand col text-center text-white">Laptop Gaming</a>
-            </nav>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">FJR Master</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    <!-- menggunakan card -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <div class="admin">
+                    <li class="nav-item">
+                        <a class="nav-link" href="php/admin.php">Admin</a>
+                    </li>
+                </div>
+                </ul>
+                <form action="" method="get" class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" name="keyword" placeholder="Cari" aria-label="Cari" autofocus>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="cari">Cari</button>
+                </form>
+            </div>
+    </div>
+</nav>
+
+        <!-- menggunakan card -->
         <div class="container">
-            <a href="php/admin.php">
-                <button>Ke halaman admin</button>
-            </a>
-
-            <form action="" method="get">
-                <input type="text" name="keyword" autofocus>
-                <button type="submit" name="cari">Cari</button>
-            </form>
-
             <div class="row">
-
-            <?php if(empty($leptop)) :?>
-                    
-                <h1>Data tidak ditemukan</h1>
-                    
-            <?php else :?>
-
-                <?php foreach ($leptop as $elek) :?>
-                    <div class="col-sm-6">
-                        <div class="card">
-                            <div class="card-body" style="">
-                                <h5 class="card-title"><?= $elek["tipe"] ?></h5>
-                                    <a href="php/detail.php?id=<?= $elek['id'] ?>" class="btn btn-primary">pilih</a>
+                <?php if(empty($leptop)) :?>
+                    <h1>Data tidak ditemukan</h1>
+                <?php else :?>
+            
+                    <?php foreach ($leptop as $elek) :?>
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body" style="">
+                                    <h5 class="card-title"><?= $elek["tipe"] ?></h5>
+                                        <a href="php/detail.php?id=<?= $elek['id'] ?>" class="btn btn-primary">pilih</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif;?>
+                    <?php endforeach; ?>
+                <?php endif;?>
             </div>
-            
         </div>
 
    
