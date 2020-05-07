43e1bb1935b2578b74ae674fe146525172d0b069
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2020-05-07 17:12:27 +0700

    updeat

---
+++ index.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+    <!-- css bootstrap -->
+    <link rel="stylesheet" href="tubes/css/bootstrap.min.css">
+
+    <!-- my fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <title>Index</title>
+    <style>
+    .container .navbar-brand{
+        font-family: 'Permanent Marker', cursive;
+        font-size: 40px;
+        color:white;
+    }
+    </style>
+    
+
+
+</head>
+<body style="background-color: black;">
+    
+    <nav class="navbar navbar-expand-lg ">
+        <div class="container justify-content-center">
+            <a class="navbar-brand" href="#"><b>FJRMaster</b></a>
+                <!-- Example single danger button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Action
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Separated link</a>
+  </div>
+</div>
+        </div>
+    </nav>
+
+
+    <div class="jumbotron jumbotron-fluid" style="background-image: url(tubes/assets/img/RGB.png); height:700px ;">
+        <div class="container">
+            <div class="container" >
+                <div class="card border-success mb-3" style="max-width: 18rem; margin-left: 390px; background-color: black;">
+                    <div class="card-header" align="center" style="font-family: 'Permanent Marker', cursive; color:white;">Link menuju praktikum</div>
+                        <div class="card-body text-success" style="height: 250px;">
+                                <!-- Example single danger button -->
+                                    <div class="btn-group">
+                                    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Action
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Separated link</a>
+                                    </div>
+                                    </div>
+                    </div>
+                    
+                    <div class="card-header" align="center" style="font-family: 'Permanent Marker', cursive; color:white;">Link menuju tugas besar</div>
+                        <div class="card-body text-success">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="tubes/js/jquery-3.5.0.min.js"></script>
+<script src="tubes/js/bootstrap.min.js"></script>
+</body>
+</html>
