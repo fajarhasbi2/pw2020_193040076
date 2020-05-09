dabfccf7875cdad738fa8c4cc3e5307bef82c3eb
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2020-05-09 16:03:11 +0700

    penambahan index

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
     <!-- my fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Galada&family=Playfair+Display&display=swap" rel="stylesheet">
 
     <title>Index</title>
     <style>
@@ -13,6 +14,23 @@
         font-size: 40px;
         color:white;
     }
+    .jumbotron{
+        background-size: cover;
+    height:700px;
+    text-align: center;
+    position: relative;
+    margin-top: -85px;
+    margin-bottom: -100px;
+    }
+    .navbar{
+    position: relative;
+    z-index: 1;
+    }
+
+  .jumbotron .container{
+    z-index: 1;
+    position: relative;}
+
     </style>
     
 
@@ -23,52 +41,60 @@
     <nav class="navbar navbar-expand-lg ">
         <div class="container justify-content-center">
             <a class="navbar-brand" href="#"><b>FJRMaster</b></a>
-                <!-- Example single danger button -->
-<div class="btn-group">
-  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">Separated link</a>
-  </div>
-</div>
+                
+  
         </div>
     </nav>
 
 
-    <div class="jumbotron jumbotron-fluid" style="background-image: url(tubes/assets/img/RGB.png); height:700px ;">
-        <div class="container">
-            <div class="container" >
-                <div class="card border-success mb-3" style="max-width: 18rem; margin-left: 390px; background-color: black;">
-                    <div class="card-header" align="center" style="font-family: 'Permanent Marker', cursive; color:white;">Link menuju praktikum</div>
-                        <div class="card-body text-success" style="height: 250px;">
-                                <!-- Example single danger button -->
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Action
-                                    </button>
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Separated link</a>
-                                    </div>
-                                    </div>
-                    </div>
-                    
-                    <div class="card-header" align="center" style="font-family: 'Permanent Marker', cursive; color:white;">Link menuju tugas besar</div>
-                        <div class="card-body text-success">
-                        
+    <div class="jumbotron jumbotron-fluid" style="background-image: url(tubes/assets/img/RGB.png); height:1000px ;">
+        <div class="container" >
+        <div class="card-deck" >
+            <div class="card border-success" style="background-color: black;">
+                <div class="card-body">
+                <h5 class="card-title" style="font-family: 'Galada', cursive; color:white;">P5_PW_193040076</h5>
+                    <div class="btn-group-vertical">
+                        <a href="praktikum/P5_PW_193040076/latihan5a"><button type="button" class="btn btn-outline-success">Latihan 5a</button></a><br>
+                        <a href="praktikum/P5_PW_193040076/latihan5b"><button type="button" class="btn btn-outline-success">Latihan 5b</button></a><br>
+                        <a href="praktikum/P5_PW_193040076/latihan5c"><button type="button" class="btn btn-outline-success">Latihan 5c</button></a><br>
                     </div>
                 </div>
             </div>
+            <div class="card border-success" style="background-color: black;">
+                <div class="card-body">
+                <h5 class="card-title" style="font-family: 'Galada', cursive; color:white;">P6_PW_193040076</h5>
+                    <div class="btn-group-vertical">
+                        <a href="praktikum/P6_PW_193040076/latihan6a"><button type="button" class="btn btn-outline-success">Latihan 6a</button></a><br>
+                        <a href="praktikum/P6_PW_193040076/latihan6b"><button type="button" class="btn btn-outline-success">Latihan 6b</button></a><br>
+                        <a href="praktikum/P6_PW_193040076/latihan6c"><button type="button" class="btn btn-outline-success">Latihan 6c</button></a><br>
+                        <a href="praktikum/P6_PW_193040076/latihan6d"><button type="button" class="btn btn-outline-success">Latihan 6d</button></a><br>
+                        <a href="praktikum/P6_PW_193040076/latihan6e"><button type="button" class="btn btn-outline-success">Latihan 6e</button></a><br>
+                        <a href="praktikum/P6_PW_193040076/tugas"><button type="button" class="btn btn-outline-success">Tugas</button></a>
+                    </div>
+                </div>
+            </div>
+            <div class="card border-success" style="background-color: black;">
+                <div class="card-body">
+                <h5 class="card-title" style="font-family: 'Galada', cursive; color:white;">P7_PW_193040076</h5>
+                    <div class="btn-group-vertical">
+                        <a href="praktikum/P7_PW_193040076/latihan5a"><button type="button" class="btn btn-outline-success">Latihan 7a</button></a><br>
+                        <a href="praktikum/P7_PW_193040076/latihan5b"><button type="button" class="btn btn-outline-success">Latihan 7b</button></a><br>
+                        <a href="praktikum/P7_PW_193040076/latihan5c"><button type="button" class="btn btn-outline-success">Latihan 7c</button></a><br>
+                    </div>
+                </div>
+            </div>
+            <div class="card border-success" style="background-color: black;">
+                <div class="card-body">
+                <h5 class="card-title" style="font-family: 'Galada', cursive; color:white;">TUGAS BESAR</h5>
+                    <div class="btn-group-vertical">
+                        <a href="tubes"><button type="button" class="btn btn-outline-success">TUBES</button></a><br>
+                </div>
+            </div>
+            </div>
         </div>
     </div>
+
+    
 
 
 
@@ -77,9 +103,6 @@
 
 
     
-
-
-
 
 
 
